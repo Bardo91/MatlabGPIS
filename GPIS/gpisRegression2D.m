70f07113d00ce37c600d04dd2b65151e64388d24
Bardo91 <19bardo91@gmail.com>
2016-04-22 17:24:25 +1000

    GPIS regression in 2D and plotting variances in 3d

---
+++ GPIS/gpisRegression2D.m
@@ -0,0 +1,64 @@
+
+% GPIS learning
+% 1D example, zero mean
+
+close all; clear all; clc;
+
+X = [0,0;
+     1,0;
+     1,1;
+     0,1]';
+
+m = 4; 
+
+[Xg,Yg] = meshgrid(-2:0.1:2,-2:0.1:2);
+[d1,d2] = size(Xg);
+Xs = [reshape(Xg,d1*d2,1),reshape(Yg,d1*d2,1)]';
+n = length(Xs);
+
+sigma = 1;
+gamma = 1;
+K = ComputeFullKder(sigma, gamma, X, 0, 0);
+Ks = ComputeKderX1X2(sigma, gamma, Xs, X);
+Kss = ComputeKderX1X2(sigma, gamma, Xs, Xs);
+
+figure();
+imagesc(K);
+colorbar;
+
+figure();
+imagesc(Ks);
+colorbar;
+
+figure();
+imagesc(Kss);
+colorbar;
+
+Ks = Ks';
+
+f = [0, -1, -1, 0, 1, -1, 0, 1, 1, 0, -1, 1]';
+fs = 0 + Ks'*inv(K)*(f);
+sig = Kss' - Ks'*inv(K)*Ks;
+
+figure();
+imagesc(sig);
+colorbar;
+
+figure();
+hold on;
+plot(X(1,:), X(2,:), 'r*');
+
+d = diag(sig);
+devFsP = fs + d;
+devFsP = reshape(devFsP(1:3:end),d1,d2);
+
+devFsN = fs - d;
+devFsN = reshape(devFsN(1:3:end),d1,d2);
+
+Fs = reshape(fs(1:3:end),d1,d2);
+figure();
+surf(Xg,Yg,Fs);
+hold on
+surf(Xg,Yg,devFsP);
+surf(Xg,Yg,devFsN);
+axis([-2 0.5 -2 2 -1 1]);

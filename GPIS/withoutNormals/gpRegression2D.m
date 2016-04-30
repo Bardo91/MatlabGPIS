99c17c3fc0c44e7c68d9191b2b9e71927a502d4e
Bardo91 <19bardo91@gmail.com>
2016-04-30 23:16:47 +1000

    Added noise and showing the "inside probability"

--- GPIS/withoutNormals/gpRegression2D.m
+++ GPIS/withoutNormals/gpRegression2D.m
@@ -6,23 +6,25 @@ close all; clear all; clc;
 X = [-0.5,-0.5;
      -0.5,0.5;
      0.5,0.5;
-     0.5,-0.5]';
-%      0.0,0.707,
-%      0.707,-0.0,
-%      -0.707,0.0,
-%      0.0, -0.707]';
+     0.5,-0.5;
+%      0.3,1;
+%      00.5,-1]';
+     0.0,0.707;
+     0.707,-0.0;
+     -0.707,0.0;
+     0.0, -0.707]';
  
 m = length(X); 
 
 f = zeros(m,1);
 
-[Xg,Yg] = meshgrid(-1:0.1:1,-1:0.1:1);
+[Xg,Yg] = meshgrid(-1.5:0.1:1.5,-1.5:0.1:1.5);
 [d1,d2] = size(Xg);
 Xs = [reshape(Xg,d1*d2,1),reshape(Yg,d1*d2,1)]';
 n = length(Xs);
 
 sigma = 1;
-gamma = 0.1;
+gamma = 0.5;
 
 kernel = @(x,y)(sigma^2 * exp(-1/2 * gamma *(x - y)'*(x - y)));
 
@@ -30,11 +32,14 @@ R = 1;
 cen = [0.0, 0.0]';
 mean = @(x) 1/2/R*((x-cen)'*(x-cen) - R*2);
 
-
+sigmaNoise = 0.5;
 K = [];
 for i = 1:m
     for j = 1:m
        K(i,j) = kernel(X(:,i), X(:,j));
+       if(i == j)
+           K(i,j) = K(i,j) + sigmaNoise*sigmaNoise;
+       end
     end
 end
 
@@ -51,6 +56,9 @@ Kss = [];
 for i = 1:n
     for j = 1:n
        Kss(i,j) = kernel(Xs(:,i), Xs(:,j));
+       if(i == j)
+           Kss(i,j) = Kss(i,j) + sigmaNoise*sigmaNoise;
+       end
     end
 end
 
@@ -96,12 +104,31 @@ devFsN = reshape(devFsN,d1,d2);
 Fs = reshape(fs,d1,d2);
 figure();
 hold on;
-plot(X(1,:), X(2,:), 'r*');
+plot(X(1,:), X(2,:), 'r*', 'MarkerSize',6);
 surf(Xg,Yg,Fs);
+contour(Xg,Yg,Fs,[0 0], 'LineWidth',2,'color', 'r');
+
+
+figure();
+hold on;
+plot(X(1,:), X(2,:), 'r*', 'MarkerSize',6);
+surf(Xg,Yg,Fs);
+contour(Xg,Yg,Fs,[0 0], 'LineWidth',2,'color', 'r');
 surf(Xg,Yg,devFsN);
 surf(Xg,Yg,devFsP);
+axis([-1.5 0 -1.5 1.5])
 
 figure();
 hold on;
 plot(X(1,:), X(2,:), 'r*');
-contour(Xg,Yg,Fs)
+contour(Xg,Yg,Fs);
+
+% Compute the inside probability.
+cdfBell = @(x) 0.5.*(1 + sign(x).*sqrt(1 - exp(-2/pi.*x.*x)));
+
+D = reshape(d, d1,d2);
+prob = cdfBell((0-Fs)./D);
+figure();
+hold on;
+surf(Xg,Yg,prob);
+

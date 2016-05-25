53b3a687ba4d3be6352f423d6a7a5ade33569a77
Bardo91 <19bardo91@gmail.com>
2016-05-25 16:54:55 +1000

    working version with plotting of probs and surfaces efficiently

--- GPIS/withNormals/ComputeFplus.m
+++ GPIS/withNormals/ComputeFplus.m
@@ -1,38 +1,18 @@
-function Fplus = ComputeFplus(PartMeans,SurfNormals,Object,cx,cy,cz,a,b,c)
+function Fplus = ComputeFplus(locations,surfNormals,meanValue,meanGrad)
 
-[D,~] = size(PartMeans);
-MeanGrad = ComputeMeanGrad(PartMeans,Object,cx,cy,cz,a,b,c);
+[D,~] = size(locations);
+MeanGrad = ComputeMeanGrad(locations,meanValue,meanGrad);
 
-MeanGradData(2:D+1,:) = SurfNormals;
+MeanGradData(2:D+1,:) = surfNormals;
 Fplus = MeanGradData(:) - MeanGrad;
 
-function Fplus = ComputeMeanGrad(PartMeans,Object,cx,cy,cz,a,b,c)
+function Fplus = ComputeMeanGrad(locations,meanValue,meanGrad)
 
-[D,N] = size(PartMeans);
-Loc = repmat(Object.Loc,[1 N]);
+[D,N] = size(locations);
 
 Fplus = zeros(D + 1,N);
-
-
-% mean = @(x,Loc)(1/(2 * R) * (sum((x - Loc).^2,1) - R^2));
-% meander = @(x,Loc)(1/R * (x - Loc));
-
-% load Ellipse_plot/semi_axis
-
-% 
-% Rx = @(ang)([1 0 0 ; 0 cos(ang) sin(ang); 0, -sin(ang) cos(ang)]);
-% Ry = @(ang)([cos(ang)  0 sin(ang) ; 0 1 0 ; -sin(ang) 0 cos(ang)]);
-% Rz = @(ang)([cos(ang) sin(ang) 0; -sin(ang) cos(ang) 0; 0 0 1]);
-
-A = diag([1/a^2,1/b^2,1/c^2]) ;
-
-mean = @(x,Loc)((x - Loc)'* A * (x - Loc) - 1)*a/2;
-meander = @(x,Loc)((A' + A)*(x - Loc))*a/2;
-
-Fplus(1,:) = diag(mean(PartMeans,Loc));
-Fplus(2:D+1,:) = meander(PartMeans,Loc);
+for n = 1:N
+    Fplus(1,n) = meanValue(locations(:,n));
+    Fplus(2:D+1,n) = meanGrad(locations(:,n));
+end
 Fplus = Fplus(:);
-
-
-
-
